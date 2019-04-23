4b73d9c9dce4f783e6453721ef9e35c9163aae5a
Ioana Ciripan <ioanarciripan@gmail.com>
2019-04-23 16:44:40 +0300

    first

---
+++ src/enums/Domains.java
@@ -0,0 +1,32 @@
+package enums;
+
+public enum Domains {
+        INFORMATICA(0),
+        MATEMATICA(1),
+        LITERATURA(2),
+        FIZICA(3),
+        MECANICA(4);
+
+        private Integer value;
+
+
+         Domains(Integer value){
+           this.value = value;
+        }
+
+
+         public static Domains fromValue(int value) throws IllegalArgumentException {
+                try {
+                    return Domains.values()[value];
+                } catch(ArrayIndexOutOfBoundsException e) {
+                throw new IllegalArgumentException("Unknown enum value :"+ value);
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ src/pakage1/Test1.java
@@ -0,0 +1,42 @@
+package pakage1;
+
+import enums.Domains;
+import models.Book;
+import models.Client;
+
+import java.sql.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Test1 {
+    public  static  void main(String[] args) {
+// MariaDB Connection localhost, 3306;
+
+
+
+
+
+/*
+        int value = 1;
+        Domains myDomain = Domains.values()[value];
+
+
+        System.out.println(myDomain.fromValue(value));
+
+*/
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
